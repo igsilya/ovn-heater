igsilya/ovn-heater

commit 4ad22627749c0a22ce79c4d7a94fa9753dc5ff25

Use the first IP address in the node_net for the tester.

ovn-tester/ovn_tester.py
@@ -286,7 +286,7 @@ def configure_tests(yaml, central_node, worker_nodes, global_cfg):
 
 def create_nodes(cluster_config, central, workers):
     mgmt_net = cluster_config.node_net
-    mgmt_ip = mgmt_net.ip + 1
+    mgmt_ip = mgmt_net.ip + 2
     internal_net = cluster_config.internal_net
     external_net = cluster_config.external_net
     gw_net = cluster_config.gw_net

do.sh
@@ -356,20 +356,11 @@ function mine_data() {
 function get_tester_ip() {
     local test_file=$1
 
+    # The tester gets the first IP address in the configured node_net.
     node_net=$(${ovn_fmn_get} ${test_file} cluster node_net --default=192.16.0.0/16)
     node_cidr=${node_net#*/}
     node_ip=${node_net%/*}
     ip_index=1
-    clustered_db=$(${ovn_fmn_get} ${test_file} cluster clustered_db --default=True)
-    # The yaml loader normalizes "yes" and "no" values for clustered_db into
-    # "True" and "False"
-    if [ "${clustered_db}" = "True" ]; then
-        (( ip_index += 3 ))
-    else
-        (( ip_index += 1 ))
-    fi
-    n_relays=$(${ovn_fmn_get} ${test_file} cluster n_relays --default=0)
-    (( ip_index += ${n_relays} ))
     tester_ip=$(${ovn_fmn_ip} ${node_net} ${node_ip} ${ip_index})
     echo "${tester_ip}/${node_cidr}"
 }

commit ae659b9cf25f29a22fa3e1de49c07da0ded747d3

ovn-tester: Fix miscalculated node remotes.
ovn-tester is currently using .1, .2 and .3 IP addresses as ovn-remote
while actual database servers are on .2, .3 and .4.  This is happening
because calculate_default_node_remotes() doesn't take into account
the tester itself that owns the .1 address.

Fixes: 4ad22627749c ("Use the first IP address in the node_net for the tester.")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

ovn-tester/ovn_tester.py
@@ -80,6 +80,8 @@ ClusterBringupCfg = namedtuple('ClusterBringupCfg', ['n_pods_per_node'])
 
 def calculate_default_node_remotes(net, clustered, n_relays, enable_ssl):
     ip_gen = net.iter_hosts()
+    # The first IP is assigned to the tester itself, skip it.
+    next(ip_gen)
     if n_relays > 0:
         skip = 3 if clustered else 1
         for _ in range(0, skip):